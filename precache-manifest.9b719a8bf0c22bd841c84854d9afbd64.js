4c4501b252345d3a6306d2241232ea82efd2ca37
ahmed9sameh <ahmed9sameh@gmail.com>
2020-07-16 15:19:56 +0200

    Updates

---
+++ precache-manifest.9b719a8bf0c22bd841c84854d9afbd64.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f210bb00034634a3aae3e89947fb0b1",
+    "url": "/FrontEnd---Spotify-clone/index.html"
+  },
+  {
+    "revision": "459e0ee599aa59649a36",
+    "url": "/FrontEnd---Spotify-clone/static/css/2.a2c668c0.chunk.css"
+  },
+  {
+    "revision": "a8bfabb76f3c8cce58fa",
+    "url": "/FrontEnd---Spotify-clone/static/css/main.65c82f7a.chunk.css"
+  },
+  {
+    "revision": "459e0ee599aa59649a36",
+    "url": "/FrontEnd---Spotify-clone/static/js/2.c8ea1e70.chunk.js"
+  },
+  {
+    "revision": "d0201696ef9d8d0d463f3917e3ecb7f2",
+    "url": "/FrontEnd---Spotify-clone/static/js/2.c8ea1e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8bfabb76f3c8cce58fa",
+    "url": "/FrontEnd---Spotify-clone/static/js/main.4db46810.chunk.js"
+  },
+  {
+    "revision": "44d526320e4b33098a90",
+    "url": "/FrontEnd---Spotify-clone/static/js/runtime-main.c13a1728.js"
+  },
+  {
+    "revision": "e58e9fe23477d7d94830ad78fb354505",
+    "url": "/FrontEnd---Spotify-clone/static/media/Amr.e58e9fe2.jfif"
+  },
+  {
+    "revision": "cd09c9012d7de8e989a183370628db33",
+    "url": "/FrontEnd---Spotify-clone/static/media/Arabish.cd09c901.jfif"
+  },
+  {
+    "revision": "4fbdfcf31d63bea24bd4bd550697a4a7",
+    "url": "/FrontEnd---Spotify-clone/static/media/Billie_Eilish.4fbdfcf3.jfif"
+  },
+  {
+    "revision": "ac12d711086d804fefeeeab63190d901",
+    "url": "/FrontEnd---Spotify-clone/static/media/Bruno_Mars.ac12d711.jfif"
+  },
+  {
+    "revision": "2775524dbeb8f08a3841e10e0df108e4",
+    "url": "/FrontEnd---Spotify-clone/static/media/Drake.2775524d.jfif"
+  },
+  {
+    "revision": "b177e77ea5dcb09381239bba39855bd8",
+    "url": "/FrontEnd---Spotify-clone/static/media/Elissa.b177e77e.jfif"
+  },
+  {
+    "revision": "fd4d01f43caf7e7dc0d4dc31f17c221d",
+    "url": "/FrontEnd---Spotify-clone/static/media/Halsey.fd4d01f4.jfif"
+  },
+  {
+    "revision": "95bcd2d703b4e53a21a4116f23d40bf0",
+    "url": "/FrontEnd---Spotify-clone/static/media/Hamaki.95bcd2d7.jfif"
+  },
+  {
+    "revision": "9dc659eab77bccf6755df55dc1579270",
+    "url": "/FrontEnd---Spotify-clone/static/media/Latifa.9dc659ea.jfif"
+  },
+  {
+    "revision": "81b6f8540596223b9cabcae238ea7cf1",
+    "url": "/FrontEnd---Spotify-clone/static/media/Maroon5.81b6f854.jfif"
+  },
+  {
+    "revision": "9d6c2205592179b0ae43b26293bb05c5",
+    "url": "/FrontEnd---Spotify-clone/static/media/Sam.9d6c2205.jfif"
+  },
+  {
+    "revision": "0fd1fc0fe0e1cf2ab0d4344ff10f767d",
+    "url": "/FrontEnd---Spotify-clone/static/media/Shawn_Mendes.0fd1fc0f.jfif"
+  },
+  {
+    "revision": "f5bf7e89dd3c090b0705efd1484c1403",
+    "url": "/FrontEnd---Spotify-clone/static/media/Sia.f5bf7e89.jpg"
+  },
+  {
+    "revision": "b4857bdb5a2ff5d358d83b60a395e095",
+    "url": "/FrontEnd---Spotify-clone/static/media/Talyor_Swift.b4857bdb.jfif"
+  },
+  {
+    "revision": "70ad13d4a7e54f7e2773399e63efb5a7",
+    "url": "/FrontEnd---Spotify-clone/static/media/Tamer_Ashour.70ad13d4.jfif"
+  },
+  {
+    "revision": "6b251762da09e56bb6a947c1ad9dcec9",
+    "url": "/FrontEnd---Spotify-clone/static/media/The_Weekend.6b251762.jfif"
+  },
+  {
+    "revision": "2f0262fd249b2a4762757c66d0df8a5d",
+    "url": "/FrontEnd---Spotify-clone/static/media/home.2f0262fd.png"
+  },
+  {
+    "revision": "c97391051b6b4cbe34d7a1bfe040d88d",
+    "url": "/FrontEnd---Spotify-clone/static/media/logo.c9739105.png"
+  },
+  {
+    "revision": "51fd4b226ddbb302ba26bac1f626f5d4",
+    "url": "/FrontEnd---Spotify-clone/static/media/otlob.51fd4b22.jpg"
+  },
+  {
+    "revision": "9290857b655e291605485227852232cc",
+    "url": "/FrontEnd---Spotify-clone/static/media/souq.9290857b.jpg"
+  },
+  {
+    "revision": "bc4ade66fc0051742e125cc28f6bd735",
+    "url": "/FrontEnd---Spotify-clone/static/media/uber.bc4ade66.jpg"
+  }
+]);
